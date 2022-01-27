22d1d97a3127cff5030d58dead143c052354931d
unknown <tamiloreolaniyi@gmail.com>
2022-01-27 07:36:53 +0100

    Ratings and PropTypes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <Navbar.Brand href="/">DarkMan Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto '>
+          <Nav className='ml-auto '>
             <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
             <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
           </Nav>
