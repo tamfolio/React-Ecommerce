024c70e802312bb16ea9629e7a4f41e80a59d126
unknown <tamiloreolaniyi@gmail.com>
2022-01-27 19:03:26 +0100

    Project Routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import { Container} from 'react-bootstrap'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import HomeScreen from './Screens/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
       <main>
       <Container>
-      <HomeScreen/>
+      <Route exact path='/' component={HomeScreen}/>
+      <Route path='/product/:id' component={ProductScreen}/>
       </Container>
       </main>
       <Footer/>
       
     </div>
+    </Router>
   );
 }
 
