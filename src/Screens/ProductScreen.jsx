63404df1b59401e3d7eb9448c00a96bc388b9e6f
unknown <tamiloreolaniyi@gmail.com>
2022-01-28 07:02:42 +0100

    Project Routing 2

--- src/Screens/ProductScreen.jsx
+++ src/Screens/ProductScreen.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
+import Rating from '../Components/Rating'
+import products from '../products';
 
-function ProductScreen() {
-  return <div>Product</div>;
+function ProductScreen({ match }) {
+
+    const product = products.find((p) => p._id === match.params.id)
+    
+  return (
+  <div>{product.name}</div>
+  );
 }
 
 export default ProductScreen;
